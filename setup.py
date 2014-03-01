319f38520b2f9880f7ac19fcf0262b3cf670fd83
Derrick Karimi <dhkarimi@sei.cmu.edu>
2014-03-01 13:30:24 -0500

    still learning how to deply dependencies from github

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         'daemon','bbot'
     ],
     dependency_links=[
-        'git+https://github.com/AlwaysTraining/bbot.git#egg=bbot']
+        'git+https://github.com/AlwaysTraining/bbot.git#egg=bbot'],
     license="BSD",
     zip_safe=False,
     keywords='bbots',
